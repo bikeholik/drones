65aafae0992f6773a277d5972398841b3d9c022c
bikeholik <bikeholik@gmail.com>
2019-06-23 15:59:00 +0200

    Getting moves and scanning cntd.

---
+++ src/main/java/com/github/bikeholik/drones/drone/DroneTask.java
@@ -0,0 +1,51 @@
+package com.github.bikeholik.drones.drone;
+
+import com.github.bikeholik.drones.common.Move;
+import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.Duration;
+import java.util.Objects;
+import java.util.Optional;
+
+@Builder
+@Slf4j
+class DroneTask implements Runnable {
+
+    private final DroneTaskProperties droneTaskProperties;
+    private final DroneService droneService;
+
+    @Override
+    public void run() {
+        log.info("[{}] Started with memory {} and speed {}", droneTaskProperties.getId(), droneTaskProperties.getMemorySize(), droneTaskProperties.getSpeed());
+        Move lastMove = null;
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                lastMove = droneService.fetchNextMovesAndScan(lastMove, droneTaskProperties, this::simulateFlight)
+                        .or(() -> {
+                            log.debug("[{}] Wait as there was no reply", droneTaskProperties.getId());
+                            simulateFlight(Duration.ofSeconds(5L));
+                            return Optional.empty();
+                        })
+                        .orElse(lastMove);
+            } catch (Exception e) {
+                if (Objects.nonNull(e.getCause()) && e.getCause() instanceof InterruptedException) {
+                    break;
+                } else {
+                    log.error("[{}] Unexpected error occurred", droneTaskProperties.getId(), e);
+                }
+            }
+        }
+        log.debug("[{}] Exiting...", droneTaskProperties.getId());
+    }
+
+    private void simulateFlight(Duration duration) {
+        log.debug("[{}] Sleep for {}", droneTaskProperties.getId(), duration);
+        try {
+            Thread.sleep(duration.toMillis());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
